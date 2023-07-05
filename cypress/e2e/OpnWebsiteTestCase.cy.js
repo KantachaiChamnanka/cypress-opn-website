f6ed7a7b64fac0782ada823388b542e03537f383
KantachaiChamnanka <kantachai.c@opn.ooo>
2023-07-05 13:24:07 +0700

    add more and try push

---
+++ cypress/e2e/OpnWebsiteTestCase.cy.js
@@ -0,0 +1,133 @@
+describe("template spec", () => {
+  it(' "Access to Opn website(Homepage)', () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/")
+  })
+
+  it("Access to Opn pro", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/opn-pro/")
+  })
+
+  it("Access to Opn mint", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/opn-mint/"
+    )
+  })
+
+  it("Access to Opn tag", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/opn-tag/"
+    )
+  })
+
+  it("Access to Accept payment", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/accept-payment/"
+    )
+  })
+
+  it("Access to Manage transactions", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/manage-transaction/"
+    )
+  })
+
+  it("Access to Send payouts ", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/send-payouts/"
+    )
+  })
+
+  it("Access to Manage-transaction/#protect-revenue ", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/manage-transaction/#protect-revenue"
+    )
+  })
+
+  it("Access to Card issuing ", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/card-issuing/"
+    )
+  })
+
+  it("Access to Acquiring", () => {
+    cy.visit(
+      "https://opn-website.staging-internal.opn.dev/th-en/products/acquiring/"
+    )
+  })
+
+  it("Access to About us", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/about-us/")
+  })
+
+  it("Access to News", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/news/")
+  })
+
+  it("Access to Get started", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/get-started/")
+  })
+
+  it("Access to Pricing", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/pricing/")
+  })
+
+  it("Access to Contact us", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/contact-us/")
+  })
+
+  it("Access to Contact sale", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/contact-us/sales/")
+  })
+
+  it("Access to Contact support", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/contact-us/support/")
+  })
+
+  it("Access to Contact partnership", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/contact-us/partnership/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+
+  it("Access to Digital menu", () => {
+    cy.visit("https://opn-website.staging-internal.opn.dev/th-en/products/digital-menu/")
+  })
+})
--- cypress/e2e/OpnWetsiteTestCase.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it(' "Access to Opn website(Homepage)', () => {
-    cy.visit('https://opn-website.staging-internal.opn.dev/th-en/')
-  })
-  it('Access to Opn pro ', () => {
-    cy.visit('https://opn-website.staging-internal.opn.dev/th-en/opn-pro/')
-  })
-})
