63c194004ff78b40e7d99bcb6c28a3ffee9c1711
KantachaiChamnanka <kantachai.c@opn.ooo>
2023-07-05 11:29:58 +0700

    add Access to opn pro test

--- cypress/e2e/OpnWetsiteTestCase.cy.js
+++ cypress/e2e/OpnWetsiteTestCase.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it(' "Access to Opn website(Homepage)', () => {
     cy.visit('https://opn-website.staging-internal.opn.dev/th-en/')
+  })
+  it('Access to Opn pro ', () => {
+    cy.visit('https://opn-website.staging-internal.opn.dev/th-en/opn-pro/')
   })
 })
